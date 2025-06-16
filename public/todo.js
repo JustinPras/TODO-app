0508a1c61014bbb423f1333c2e859c9ad89b7352
Justin Praseuthsouk <justinponpras@gmail.com>
2025-06-16 13:14:23 +1000

    fix: create tasks - popup to create a task

--- public/todo.js
+++ public/todo.js
@@ -39,6 +39,7 @@ async function createTask() {
 
     body.value = '';
     const taskID = data.id;
+    closeModal(modal)
     if (taskID) {
       await getTasks();
     }
@@ -236,4 +237,46 @@ function resetTaskSelection() {
   currentTask = null;
   editTaskBtn.disabled = true;
   deleteTaskBtn.disabled = true;
+}
+
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
+
+openModalButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  })
+})
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".modal.active");
+  modals.forEach(modal => {
+    closeModal(modal);
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) {
+    return;
+  }
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == null) {
+    console.log("null modal to close")
+    return;
+  }
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
 }
